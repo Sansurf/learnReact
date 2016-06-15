0ab471ccc4a2fe985c2494e57e8824884a3e3829
Eliseev Alexandr <sansurf@inbox.ru>
2016-06-16 02:18:48 +0500

    Закончил на Десктопе "Продвинутое использование state"

--- js/app.js
+++ js/app.js
@@ -1,26 +1,61 @@
 var my_news = [
 	{
 		author: "Author1",
-		text: "Text of author1"
+		text: "Text of Author1",
+		bigText: "Big Text of Author1"
 	},
 	{
 		author: "Author2",
-		text: "Text of Author2"
+		text: "Text of Author2",
+		bigText: "Big Text of Author2"
 	},
 	{
 		author: "Author3",
-		text: "Text of Author3"
+		text: "Text of Author3",
+		bigText: "Big Text of Author3"
 	}
 ];
 
 var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape ({
+			author: React.PropTypes.string.isRequired,
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired
+		})
+	},
+
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+
+	readMoreClick: function(e) {
+		e.preventDefault();
+		this.setState({visible: true});
+	},
+
 	render: function() {
 		var author = this.props.data.author,
-			text = this.props.data.text;
+			text = this.props.data.text,
+			bigText = this.props.data.bigText,
+			visible = this.state.visible;
+
 		return (
 			<div className="article">
 				<p className="news_author">{author}</p>
 				<p className="news_text">{text}</p>
+
+			{/*Показывать ссылку, если visible === false*/}
+				<a href="#" 
+					onClick={this.readMoreClick} 
+					className={"news_readmore " + (visible ? "none" : '')}>
+					Подробнее
+				</a>
+
+			{/*Не показывать bigText, если visible === true и ссылка видна*/}
+				<p className={"news_bigtext " + (visible ? '' : "none")}>{bigText}</p>
 			</div>
 		)
 	}
@@ -30,10 +65,11 @@ var News = React.createClass({
 	propTypes: {
 		data: React.PropTypes.array.isRequired
 	},
-	
+
 	render: function() {
-		var data = this.props.data;
-		var	newsTemplate;
+		var data = this.props.data,
+			newsTemplate;
+
 		if (data.length !== 0) {
 			 newsTemplate = data.map(function(item, index) {
 				return (
@@ -49,7 +85,9 @@ var News = React.createClass({
 		return (
 			<div className="news">
 				{newsTemplate}
-				<strong className={'news_count ' + (data.length == 0? 'none' : '')}>Количество новостей: {data.length}</strong>
+				<strong className={'news_count ' + (data.length == 0? 'none' : '')}>
+					Количество новостей: {data.length}
+				</strong>
 			</div>
 		);
 	}
@@ -60,7 +98,7 @@ var App = React.createClass({
 		return (
 			<div className="app">
 				<h3>Новости</h3>
-				<News data = {my_news} /> {/*Добавили свойство my_news*/}
+				<News data = {my_news} /> {/*Добавили свойство data*/}
 			</div>
 		);
 	}
